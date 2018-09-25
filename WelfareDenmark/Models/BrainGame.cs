ee4907e76332f0336ef8cb5ac2b88a0b5f31c736
Andreas Juul Willumsen <andr532c@edu.eal.dk>
2018-09-25 11:45:05 +0200

    Add brain game model

---
+++ WelfareDenmark/Models/BrainGame.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WelfareDenmark.Models
+{
+    public class BrainGame
+    {
+        public string Name { get; set; }
+        public IEnumerable<GameResult> Results { get; set; }
+    }
+}
---
+++ WelfareDenmark/Models/GameResult.cs
@@ -0,0 +1,8 @@
+﻿namespace WelfareDenmark.Models
+{
+    public class GameResult
+    {
+        public double Score { get; set; }
+        public string Player { get; set; }
+    }
+}
