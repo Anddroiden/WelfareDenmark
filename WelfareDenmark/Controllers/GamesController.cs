09bf137b5277648323d4d33c6cab6f07417d2445
Andreas Juul Willumsen <andr532c@edu.eal.dk>
2018-09-25 12:42:11 +0200

    games controller work

--- WelfareDenmark/Controllers/GamesController.cs
+++ WelfareDenmark/Controllers/GamesController.cs
@@ -1,7 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WelfareDenmark.Data;
 
 namespace WelfareDenmark.Controllers {
     public class GamesController : Controller {
+        private readonly ApplicationDbContext _db;
+
+        public GamesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Reaction() {
             return View();
         }
@@ -9,8 +17,17 @@ namespace WelfareDenmark.Controllers {
         public IActionResult Index() {
             return View();
         }
-
+        [HttpGet]
         public IActionResult DataEntry() {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DataEntry(GameResultDTO dto) {
+            if (!ModelState.IsValid) {
+                return View();
+            }
+            var brainGame = _db.BrainGames.First(bg=>bg.Name == dto.Name);
+            _db.SaveChanges();
             return View();
         }
     }
