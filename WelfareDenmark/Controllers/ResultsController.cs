7c46fbed13ee3742f9f59e10ec1a37c6a2bf0550
kristoffer petersen <krizzmp@gmail.com>
2018-10-03 14:41:59 +0200

    only show results for the logged in user

--- WelfareDenmark/Controllers/ResultsController.cs
+++ WelfareDenmark/Controllers/ResultsController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,30 +7,29 @@ using Microsoft.EntityFrameworkCore;
 using WelfareDenmark.Data;
 using WelfareDenmark.Models;
 
-namespace WelfareDenmark.Controllers
-{
-    public class ResultsController : Controller
-    {
+namespace WelfareDenmark.Controllers {
+    public class ResultsController : Controller {
         private readonly ApplicationDbContext _db;
 
-        public ResultsController(ApplicationDbContext db)
-        {
+        public ResultsController(ApplicationDbContext db) {
             _db = db;
         }
         public IActionResult Index()
         {
             ViewData["Message"] = "Your application description page.";
 
-            var gameResults = _db.Results.Include(r=>r.BrainGame);
-            return View(new[] {
+        public IActionResult Index() {
+            var gameResults = _db.Results.Include(r => r.BrainGame).Where(r => r.Player == User.Identity.Name);
+            var results = new[] {
                 new GameResult {
-                    BrainGame = new BrainGame{Name = "Reaction"}, 
+                    BrainGame = new BrainGame {Name = "Reaction"},
                     DateTime = new DateTime(),
                     Id = 3,
                     Player = "username is cool",
-                    Score = 5867 
-                } 
-            });
+                    Score = 5867
+                }
+            };
+            return View(gameResults);
         }
     }
 }
