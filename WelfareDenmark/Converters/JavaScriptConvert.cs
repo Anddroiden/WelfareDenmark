9cfb502b8df2867cdaa7034710e6bfa07381a371
kristoffer petersen <krizzmp@gmail.com>
2018-10-10 14:30:17 +0200

    display chart of results

---
+++ WelfareDenmark/Converters/JavaScriptConvert.cs
@@ -0,0 +1,28 @@
+using System.IO;
+using Microsoft.AspNetCore.Html;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace WelfareDenmark.Converters {
+    public static class JavaScriptConvert
+    {
+        public static HtmlString SerializeObject(object value)
+        {
+            using (var stringWriter = new StringWriter())
+            using (var jsonWriter = new JsonTextWriter(stringWriter))
+            {
+                var serializer = new JsonSerializer
+                {
+                    // Let's use camelCasing as is common practice in JavaScript
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                };
+
+                // We don't want quotes around object names
+                jsonWriter.QuoteName = false;
+                serializer.Serialize(jsonWriter, value);
+
+                return new HtmlString(stringWriter.ToString());
+            }
+        }
+    }
+}
