e5d4377b6c64e96e83571a0afc50708abf603513
Andreas Juul Willumsen <andr532c@edu.eal.dk>
2018-11-22 14:28:08 +0100

    api controllers

---
+++ WelfareDenmark/APIControllers/BrainGamesController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WelfareDenmark.Data;
+using WelfareDenmark.Models;
+
+namespace WelfareDenmark.APIControllers
+{
+    [Route("api/BrainGames")]
+    [ApiController]
+    public class BrainGamesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BrainGamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/BrainGames
+        [HttpGet]
+        public IEnumerable<BrainGame> GetBrainGames()
+        {
+            return _context.BrainGames;
+        }
+
+        // GET: api/BrainGames/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBrainGame([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var brainGame = await _context.BrainGames.FindAsync(id);
+
+            if (brainGame == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(brainGame);
+        }
+
+        // PUT: api/BrainGames/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBrainGame([FromRoute] long id, [FromBody] BrainGame brainGame)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != brainGame.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(brainGame).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BrainGameExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/BrainGames
+        [HttpPost]
+        public async Task<IActionResult> PostBrainGame([FromBody] BrainGame brainGame)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.BrainGames.Add(brainGame);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBrainGame", new { id = brainGame.Id }, brainGame);
+        }
+
+        // DELETE: api/BrainGames/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBrainGame([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var brainGame = await _context.BrainGames.FindAsync(id);
+            if (brainGame == null)
+            {
+                return NotFound();
+            }
+
+            _context.BrainGames.Remove(brainGame);
+            await _context.SaveChangesAsync();
+
+            return Ok(brainGame);
+        }
+
+        private bool BrainGameExists(long id)
+        {
+            return _context.BrainGames.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ WelfareDenmark/APIControllers/GameResultController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WelfareDenmark.Data;
+using WelfareDenmark.Models;
+
+namespace WelfareDenmark.APIControllers
+{
+    [Route("api/Results")]
+    [ApiController]
+    public class GameResultController: ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GameResultController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetGameResult([FromRoute] long id)
+        {
+            var gameResult = await _context.Results.FindAsync(id);
+            if (gameResult == null)
+            {
+                return NotFound();
+            }
+            return Ok(gameResult);
+        }
+    }
+}
