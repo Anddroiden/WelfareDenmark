6f674ddc91cf9b8086d31271f6114863fb76d99c
kristoffer petersen <krizzmp@gmail.com>
2018-11-29 14:09:24 +0100

    getPatientResultsForConsultant

---
+++ WelfareDenmark/APIControllers/PatientsController.cs
@@ -0,0 +1,36 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WelfareDenmark.Data;
+using WelfareDenmark.Models;
+
+namespace WelfareDenmark.APIControllers {
+    [Route("api/patients")]
+    [ApiController]
+    public class PatientsController : ControllerBase {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public PatientsController(ApplicationDbContext dbContext, UserManager<ApplicationUser> userManager) {
+            _dbContext = dbContext;
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllPatientsForConsultant() {
+            var user = await _userManager.GetUserAsync(User);
+
+            var applicationUser = _dbContext.Users.Include(t => t.Patients).First(u => u.Id == user.Id);
+
+            var patients = applicationUser.Patients;
+            var pr = patients.Select(p => {
+                return new {
+                    Name = p.UserName,
+                    Results = _dbContext.Results.Where(result => result.Player == p.UserName)
+                };
+            });
+            return Ok(pr);
+        }
+    }
+}
