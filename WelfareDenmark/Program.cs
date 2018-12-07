0b7615dc9deac360e85b5c013a97631438cf1b97
kristoffer petersen <krizzmp@gmail.com>
2018-12-07 11:31:30 +0100

    add cert for ssl and docker

--- WelfareDenmark/Program.cs
+++ WelfareDenmark/Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.Net;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace WelfareDenmark {
     public class Program {
@@ -9,6 +11,15 @@ namespace WelfareDenmark {
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) {
             return WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options => 
+                {
+                    options.Listen(IPAddress.Any, 443, listenOptions =>
+                    {
+                        var configuration = (IConfiguration)options.ApplicationServices.GetService(typeof(IConfiguration));
+
+                        listenOptions.UseHttps("cert.pfx", configuration["certPassword"]);
+                    });
+                })
                 .UseStartup<Startup>();
         }
     }
